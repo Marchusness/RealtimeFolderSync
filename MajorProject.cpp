a1ca1f0a6420822fcbf4e1363fb54d0275f1c6b0
m-gunn <98993974+m-gunn@users.noreply.github.com>
2022-05-04 16:29:43 +0930

    boid-master

--- MajorProject.cpp
+++ MajorProject.cpp
@@ -7,3 +7,6 @@ int main()
     engine.loop();
     return 1;
 }
+
+
+// g++ -std=c++17 MajorProject.cpp -I/opt/homebrew/Cellar/sfml/2.5.1_1/include -o test -L/opt/homebrew/Cellar/sfml/2.5.1_1/lib -lsfml-graphics -lsfml-window -lsfml-system -v
