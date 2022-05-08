bd61358fa03aa73b3657ed9587dd93373f5f4565
m-gunn <98993974+m-gunn@users.noreply.github.com>
2022-05-08 21:37:39 +0930

    document

--- Scene.cpp
+++
@@ -1,5 +0,0 @@
-#include "Scene.h"
-
-Scene::~Scene() {}
-
-void Scene::handleKeyPress(sf::Event event) {}
--- boids-main/README.md
+++
@@ -1,2 +0,0 @@
-# boids
-interactive 2d flocking simulation with many customizable parameters
